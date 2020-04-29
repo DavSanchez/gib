bf5940794ea41d04a4c55cdeb71c4407c8afbe7b
David Sánchez <d.vinternatt@gmail.com>
2020-04-29 18:16:01 +0100

    Replac Travis before_deploy with external script

---
+++ ci/deploy.sh
@@ -0,0 +1,28 @@
+set -ex
+
+main () {
+    local src=$(pwd) \
+    stage= \
+    bin_name=
+    case $TRAVIS_OS_NAME in
+        linux)
+            stage=$(mktemp -d)
+            bin_name=$CRATE_NAME
+            ;;
+        osx)
+            stage=$(mktemp -d -t tmp)
+            bin=name=$CRATE_NAME
+            ;;
+        windows)
+            stage=$(mktemp -d -t tmp)
+            bin_name=$CRATE_NAME.exe
+            ;;
+    esac
+    cargo build --release
+
+    cp target/release/$CRATE_NAME $stage/
+    cd $stage
+    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TRAVIS_OS_NAME.tar.gz $bin_name
+    cd $src
+    rm -rf $stage
+}
